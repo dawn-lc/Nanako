1cb95e3ac8bc1da318a91923ddbcb8412c0ff8f1
dawn-lc <lc990930@163.com>
2022-06-05 22:35:52 +0800

    插件初版

---
+++ BasicCommand/Main.cs
@@ -0,0 +1,163 @@
+﻿using Konata.Core;
+using Konata.Core.Common;
+using Konata.Core.Events;
+using Konata.Core.Events.Model;
+using Konata.Core.Interfaces.Api;
+using Konata.Core.Message;
+using Konata.Core.Message.Model;
+using Nanako.Module;
+using Nanako.Utils;
+
+namespace BasicCommand
+{
+    public class Main : Plugin
+    {
+        public override string Name => "BasicCommand";
+
+        public override string Info => "Basic Commands";
+
+        public override async Task<bool> Process(Bot bot, BaseEvent eventSource)
+        {
+
+            switch (eventSource)
+            {
+                case GroupMessageEvent Message:
+                    if (Message.Chain.First() is TextChain GroupChain)
+                    {
+                        var Command = new Commands<string>(GroupChain.Content.ToLower());
+                        if (Command[0][..1] == "/") await bot.SendGroupMessage(Message.GroupUin, Command[0][1..].ToLower() switch
+                        {
+                            "帮助" => OnCommandHelp(),
+                            "ping" => OnCommandPing(),
+                            "状态" => OnCommandStatus(),
+                            _ => Text("未知命令")
+                        });
+                    } else if (Message.Chain.Any(p => p is ReplyChain reply) && Message.Chain.Any(p => p is AtChain at && at.AtUin == bot.Uin))
+                    {
+                        if (!Message.Chain.Any(p => p is TextChain text && text.Content.Trim() != "")) break;
+                        var Command = Message.Chain.Where(p => p is TextChain text && text.Content.Trim() != "").First() as TextChain;
+                        await bot.SendGroupMessage(Message.GroupUin, Command.Content.Trim().ToLower() switch
+                        {
+                            "帮助" => OnCommandHelp(),
+                            "ping" => OnCommandPing(),
+                            "状态" => OnCommandStatus(),
+                            _ => Text("未知命令")
+                        });
+                    }
+                    break;
+                case FriendMessageEvent Message:
+                    if (Message.Chain.First() is TextChain FriendChain)
+                    {
+                        var Command = new Commands<string>(FriendChain.Content.Trim());
+                        if (Command[0][..1] == "/") await bot.SendFriendMessage(Message.FriendUin, Command[0][1..].ToLower() switch
+                        {
+                            "帮助" => OnCommandHelp(),
+                            "ping" => OnCommandPing(),
+                            "状态" => OnCommandStatus(),
+                            "启用Bot" => await EnableBot(Convert.ToUInt32(Command[1]), Message.FriendUin) ? Text("已启用") : Text("指令执行失败, 没有足够的权限执行该命令..."),
+                            "禁用Bot" => await EnableBot(Convert.ToUInt32(Command[1]), Message.FriendUin) ? Text("已禁用") : Text("指令执行失败, 没有足够的权限执行该命令..."),
+                            _ => Text("未知命令")
+                        });
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+        
+
+        /// <summary>
+        /// Get target in group permission
+        /// </summary>
+        /// <param name="group"></param>
+        /// <param name="target"></param>
+        /// <returns>RoleType</returns>
+        public static async Task<RoleType> GetPermAsync(Bot bot, uint group, uint target)
+        {
+            return (await bot.GetGroupMemberInfo(group, target)).Role;
+        }
+
+        /// <summary>
+        /// Get bot in group permission
+        /// </summary>
+        /// <param name="bot"></param>
+        /// <param name="group"></param>
+        /// <returns>RoleType</returns>
+        public static async Task<RoleType> GetBotPermAsync(Bot bot, uint group)
+        {
+            return await GetPermAsync(bot, group, bot.Uin);
+        }
+
+        /// <summary>
+        /// On help
+        /// </summary>
+        /// <param name="chain"></param>
+        /// <returns></returns>
+        public static MessageBuilder OnCommandHelp()
+            => new MessageBuilder()
+                .Text("[Help]\n")
+                .Text("/help   Print this bot help\n")
+                .Text("/ping   Pong!\n")
+                .Text("/status   Show bot status\n")
+                .Text("/echo   Send a message");
+
+
+        /// <summary>
+        /// On status
+        /// </summary>
+        /// <param name="chain"></param>
+        /// <returns></returns>
+        public static MessageBuilder OnCommandStatus()
+        {
+            string botStatus = "";
+            if (Nanako.Program.BotList.Count < 1)
+            {
+                botStatus += "Bot does not exist!\n";
+            }
+            else
+            {
+                foreach (var bot in GetBotList())
+                {
+                    botStatus += $"[{bot.Name}({bot.Uin})]    {(bot.IsOnline() ? "Online" : "Offline")}\n";
+                }
+            }
+
+
+            return new MessageBuilder()
+                 .Text($"[{Env.Name}]\n\n")
+
+                 .Text($"[Build Info]\n")
+                 .Text($"[branch:{BuildStamp.Branch}]\n")
+                 .Text($"[commit:{BuildStamp.CommitHash[..12]}]\n")
+                 .Text($"[version:{BuildStamp.Version}]\n")
+                 .Text($"[{BuildStamp.BuildTime}]\n\n")
+                 // System status
+                 .Text($"Processed {GetMessageCounter} message(s)\n")
+
+                 .Text($"[Bot List]\n")
+                 .Text($"{botStatus}\n")
+
+                 .Text("Powered By Konata.Core");
+        }
+
+        /// <summary>
+        /// On ping me
+        /// </summary>
+        /// <param name="chain"></param>
+        /// <returns></returns>
+        public static MessageBuilder OnCommandPing()
+            => Text("Pong!");
+
+        /// <summary>
+        /// Repeat
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static MessageBuilder OnRepeat(MessageChain message)
+            => new(message);
+
+        private static MessageBuilder Text(string text)
+            => new MessageBuilder().Text(text);
+    }
+}
